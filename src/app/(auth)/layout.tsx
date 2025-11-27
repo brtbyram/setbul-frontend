ef147f999e0e890cfea1f5ff7bfa8cfc552b4686
brtbyram <brtbyram.61@gmail.com>
2025-11-27 23:14:04 +0300

    Enhance Auth Layout and Login Page with animations and improved UI elements

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,12 +1,24 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen">
-      <div className="container mx-auto px-4 py-8">
+    <div className="min-h-screen flex justify-between">
+      <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, ease: "easeOut" }}
+        className="w-full container mx-auto"
+      >
         {children}
+      </motion.div>
+      <div className="bg-neutral-800 text-white shadow-md h-screen w-10/12 lg:w-full rounded-s-2xl hidden md:flex items-center justify-center">
+        slider area
       </div>
     </div>
   );
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,56 @@
-import { LoginForm } from "@/features/auth/components/LoginForm";
+"use client";
+
+import { EyeIcon, EyeOff } from "lucide-react";
+import Link from "next/link";
+import { useState } from "react";
+import LoginForm from "@/features/auth/components/LoginForm";
+import SocialButtons from "@/features/auth/components/SocialButtons";
+
+export function CustomCheckbox({ id }: { id: string }) {
+  return (
+    <div className="flex items-center">
+      <input type="checkbox" id={id} className="hidden" />
+      <label
+        htmlFor={id}
+        className="h-6 w-6  inline-block border border-neutral-300 rounded-sm relative cursor-pointer before:content-[''] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:w-2 before:h-2 before:bg-green-600 before:hidden checked:before:block"
+      ></label>
+    </div>
+  );
+}
 
 export default function LoginPage() {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+    <div className="flex flex-col items-center justify-center h-full text-[#53546c]">
+      <Link href="/" className="mb-6 scale-120 flex font-bold rounded-3xl overflow-hidden text-neutral-50 rounded-bl-none pl-1 pr-3 border-y-neutral-50 border-8 border-green-700 bg-green-800 shadow-md shadow-green-700 -rotate-10 drop-shadow-neutral-200 drop-shadow-2xl">
+        <div translate="no" className="rotate-20 italic text-7xl leading-10">
+          {" "}
+          S
+        </div>
+        <div translate="no" className="text-7xl leading-10 font-extrabold">
+          b
+        </div>
+      </Link>
+      <h1 className="text-3xl font-semibold text-center my-4 text-black">Welcome Back to Setbul</h1>
+      <p className="text-sm text-[#909298] text-center">Enter your credentials to access your account.</p>
+
       <LoginForm />
+
+      <div className="flex items-center w-full max-w-96 my-4">
+        <div className="grow border-t border-[#e5e5e5]" />
+        <span className="shrink mx-4 text-[#909298] text-sm">Or login with</span>
+        <div className="grow border-t border-[#e5e5e5]" />
+      </div>
+
+      <SocialButtons />
+      
+      <div className="mt-4 text-sm text-[#909298]">
+        <span>Don&apos;t have an account? </span>
+        <Link href="/register" className="text-black">
+          Sign Up
+        </Link>
+      </div>
     </div>
   );
 }
